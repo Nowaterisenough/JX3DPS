7b45f5ae1dc47feac70982327928b1d081a5c6f9
NoWats <NoWaterisEnough@Outlook.com>
2022-01-27 23:24:26 +0800

    格式化常量名

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 
 #include "Version.h"
 
-const char *const name    = "JX3DPS";
-const char *const version = VERSION;
+const char *const APP_NAME    = "JX3DPS";
+const char *const APP_VERSION = VERSION;
 
 int main(int argc, char *argv[])
 {
-    std::cout << name << std::endl;
-    std::cout << version << std::endl;
+    std::cout << APP_NAME << std::endl;
+    std::cout << APP_VERSION << std::endl;
     return 0;
 }

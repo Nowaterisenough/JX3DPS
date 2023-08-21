7e1fb7111f4fd50bd9b9831a557b67861e2dae71
NoWats <NoWaterisEnough@outlook.com>
2023-08-21 20:10:15 +0800

    feat: 新增剑纯阵法配置

--- src/Class/TaiXuJianYi/TaiXuJianYi.cpp
+++ src/Class/TaiXuJianYi/TaiXuJianYi.cpp
@@ -5,7 +5,7 @@
  * Created Date: 2023-07-20 02:40:46
  * Author: 难为水
  * -----
- * Last Modified: 2023-08-21 19:28:21
+ * Last Modified: 2023-08-21 19:54:27
  * Modified By: 难为水
  * -----
  * CHANGELOG:
@@ -980,7 +980,7 @@ void Player::Init()
 
 void Player::TriggerWuYi(const Params &params)
 {
-    if (params.level > 6) {
+    if (params.level >= 6) {
         params.player->skills[SKILL_WU_WO_WU_JIAN]->AddCriticalStrikeAdditionalBasisPointInt(
             1000 * static_cast<int>(params.type));
         params.player->skills[SKILL_WU_WO_WU_JIAN]->AddCriticalStrikePowerAdditionalPercentInt(
